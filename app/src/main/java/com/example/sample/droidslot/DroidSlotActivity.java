88d06b7132f755c05820f58eb37947c105f135d3
kurupar <kurupar0715@yahoo.co.jp>
2018-07-13 22:59:34 +0900

    ドロイド君　後ろむき表示

--- app/src/main/java/com/example/sample/droidslot/DroidSlotActivity.java
+++ app/src/main/java/com/example/sample/droidslot/DroidSlotActivity.java
@@ -2,6 +2,9 @@ package com.example.sample.droidslot;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class DroidSlotActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class DroidSlotActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_droid_slot);
+        //画面上から「droidimageid1」を検出し変数にセット
+        ImageView droidImage1 = (ImageView) this.findViewById(R.id.droidimageid1);
+        int droidSide1;                     //整数を入れるための変数を宣言
+        Random r = new Random();            //Rondomクラスのオブジェクト作成
+        droidSide1 = r.nextInt(2);   //0以上２未満の整数の乱数を作成する。
+        if(droidSide1 == 0){
+            //0なら前向き
+            droidImage1.setImageResource(R.drawable.droid_front);
+        }else{
+            //それ以外は後ろ向き
+            droidImage1.setImageResource(R.drawable.droid_back);
+        }
     }
 }
---
+++ app/src/test/java/com/example/sample/droidslot/DroidSlotActivityTest.java
@@ -0,0 +1,7 @@
+package com.example.sample.droidslot;
+
+import static org.junit.Assert.*;
+
+public class DroidSlotActivityTest {
+
+}
