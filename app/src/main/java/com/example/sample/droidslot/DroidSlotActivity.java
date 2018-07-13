b8fd5069c6048497923cfac147b2ff7142a1d5bd
kurupar <kurupar0715@yahoo.co.jp>
2018-07-13 23:12:43 +0900

    ドロイド君　左右表示

--- app/src/main/java/com/example/sample/droidslot/DroidSlotActivity.java
+++ app/src/main/java/com/example/sample/droidslot/DroidSlotActivity.java
@@ -16,13 +16,33 @@ public class DroidSlotActivity extends AppCompatActivity {
         ImageView droidImage1 = (ImageView) this.findViewById(R.id.droidimageid1);
         int droidSide1;                     //整数を入れるための変数を宣言
         Random r = new Random();            //Rondomクラスのオブジェクト作成
-        droidSide1 = r.nextInt(2);   //0以上２未満の整数の乱数を作成する。
-        if(droidSide1 == 0){
-            //0なら前向き
-            droidImage1.setImageResource(R.drawable.droid_front);
-        }else{
-            //それ以外は後ろ向き
-            droidImage1.setImageResource(R.drawable.droid_back);
+        droidSide1 = r.nextInt(4);   //0以上4未満の整数の乱数を作成する。
+        //if(droidSide1 == 0){
+        //    //0なら前向き
+        //    droidImage1.setImageResource(R.drawable.droid_front);
+        //}else{
+        //   //それ以外は後ろ向き
+        //    droidImage1.setImageResource(R.drawable.droid_back);
+        //}
+        int drowbleId;  //リソースID用変数
+        switch(droidSide1){
+            case 0:
+                //0なら前向き
+                drowbleId = R.drawable.droid_front;
+                break;
+            case 1:
+                //1なら後ろ向き
+                drowbleId = R.drawable.droid_back;
+                break;
+            case 2:
+                //２なら左向き
+                drowbleId = R.drawable.droid_left;
+                break;
+            default:
+                //それ以外は右向き
+                drowbleId = R.drawable.droid_right;
         }
+        droidImage1.setImageResource(drowbleId);
+
     }
 }
