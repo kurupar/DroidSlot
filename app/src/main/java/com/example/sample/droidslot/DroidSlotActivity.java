e52ac2effd0e6ea1a1d2bcff42d26ee4b6d41049
kurupar <kurupar0715@yahoo.co.jp>
2018-07-13 22:10:14 +0900

    Initial commit

---
+++ app/src/main/java/com/example/sample/droidslot/DroidSlotActivity.java
@@ -0,0 +1,13 @@
+package com.example.sample.droidslot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DroidSlotActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_droid_slot);
+    }
+}
